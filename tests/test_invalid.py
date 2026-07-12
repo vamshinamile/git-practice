1b4d9576b75a9a08e35b6ef1019f3bba56967fd1
vamshi <vamshinamile22@gmail.com>
2026-07-12 18:28:59 +0530

    testui

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -1,12 +1,12 @@
-from pages.homepage import Homepage
+# from pages.homepage import Homepage
 
-def test_empty_cart(driver):
-    home = Homepage(driver)
+# def test_empty_cart(driver):
+#     home = Homepage(driver)
 
-    home.click_cart()
-    home.message_checking()
-    message = home.get_empty_cart_message()
+#     home.click_cart()
+#     home.message_checking()
+#     message = home.get_empty_cart_message()
 
-    print(message)
+#     print(message)
 
-    assert message == "Your Amazon Cart is empty"
+#     assert message == "Your Amazon Cart is empty"
